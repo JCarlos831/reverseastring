149c0dc5459a5f145b2bb142bd213d698aed862a
Juan Montoya <jcmontoya@gmail.com>
2017-11-08 00:38:40 +0000

    Simplify function

--- reverseAString.js
+++ reverseAString.js
@@ -1,15 +1,36 @@
+// function reverseString(str) {
+//   var strArray = [];
+//   strArray = str.split("");
+// //   console.log("splitting the string");
+// //   console.log(strArray);
+//   strArray.reverse();
+// //   console.log("after reverse");
+// //   console.log(strArray);
+//   var reverseStr = strArray.join("");
+// //   console.log("after join");
+// //   console.log(reverseStr);
+//   return reverseStr;
+// }
+
+// console.log(reverseString("Juan Carlos Montoya"));
+
+// function reverseString(str) {
+//   str = str.split("");
+// //   console.log("splitting the string");
+// //   console.log(strArray);
+//   str.reverse();
+// //   console.log("after reverse");
+// //   console.log(strArray);
+//   return str.join("");
+// //   console.log("after join");
+// //   console.log(reverseStr);
+// }
+
+// reverseString("hello");
+
+
 function reverseString(str) {
-  var strArray = [];
-  strArray = str.split("");
-//   console.log("splitting the string");
-//   console.log(strArray);
-  strArray.reverse();
-//   console.log("after reverse");
-//   console.log(strArray);
-  var reverseStr = strArray.join("");
-//   console.log("after join");
-//   console.log(reverseStr);
-  return reverseStr;
+  return str.split("").reverse().join("");
 }
 
-console.log(reverseString("Juan Carlos Montoya"));
+reverseString("hello");
